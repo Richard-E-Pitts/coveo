5501a191ca08b2d068a974c79547b54b09d13e34
Kingdom Desires <KingdomDesires@gmail.com>
2022-10-10 20:57:38 -0500

    1

--- src/components/search-box.tsx
+++ src/components/search-box.tsx
@@ -1,12 +1,15 @@
-import {SearchBox as HeadlessSearchBox} from '@coveo/headless';
-import {FunctionComponent, useEffect, useState} from 'react';
+import { SearchBox as HeadlessSearchBox } from '@coveo/headless';
+import { FunctionComponent, useEffect, useState } from 'react';
 
-interface SearchBoxProps {
+interface SearchBoxProps
+{
   controller: HeadlessSearchBox;
 }
 
-export const SearchBox: FunctionComponent<SearchBoxProps> = (props) => {
-  const {controller} = props;
+export const SearchBox: FunctionComponent<SearchBoxProps> = (props) =>
+{
+  const { controller } = props;
+  const [focused, setFocused] = useState(false);
   const [state, setState] = useState(controller.state);
 
   useEffect(() => controller.subscribe(() => setState(controller.state)), [
@@ -16,30 +19,49 @@ export const SearchBox: FunctionComponent<SearchBoxProps> = (props) => {
   const suggestionStyle = {
     cursor: 'pointer',
   };
-  
+
   return (
-     <div className="search-box">
-       <input
-         value={state.value}
-         onChange={(e) => controller.updateText(e.target.value)}
-         onKeyDown={(e) => e.key === 'Enter' && controller.submit()}
-       />
-       {state.suggestions.length > 0 && (
-         <ul>
-           {state.suggestions.map((suggestion) => {
-             const value = suggestion.rawValue;
-             return (
-               <li
-                 style={suggestionStyle}
-                 key={value}
-                 onClick={() => controller.selectSuggestion(value)}
-               >
-                  {value}
-               </li>
-             );
-           })}
-         </ul>
-       )}
-     </div>
-   );
+    <div className="search-box">
+      <input
+        value={state.value}
+        onChange={(e) => controller.updateText(e.target.value)}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        onKeyDown={(e) =>
+        {
+          if (e.key === 'Enter')
+          {
+            controller.submit();
+          } else if (e.key === 'Escape')
+          {
+            controller.clear();
+            (e.target as HTMLInputElement).blur();
           }
+        }}
+      />
+      <button onClick={() => controller.submit()}>Search</button>
+
+
+
+
+      <button onClick={() => controller.clear()}>Clear</button>
+
+      {focused && state.suggestions.length > 0 && (
+        <ul>
+          {state.suggestions.map((suggestion) =>
+          {
+            return (
+              <li
+                style={suggestionStyle}
+                key={suggestion.rawValue}
+                onMouseDown={(e) => e.preventDefault()}
+                onClick={() => controller.selectSuggestion(suggestion.rawValue)}
+                dangerouslySetInnerHTML={{ __html: suggestion.highlightedValue }}
+              ></li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+}
